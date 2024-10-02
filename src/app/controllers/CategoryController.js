bfe462a9be703e3f2798818241fa5a7412784b42
Ederl010203 <eder.luis.1996@hotmail.com>
2024-10-02 00:37:03 -0300

    Criamos a tabela de categoria com model e controller

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,35 @@
+import * as Yup from "yup"
+import Category from "../models/Category";
+
+
+class CategoryController{
+ async store(request,response){
+  const Schema = Yup.object({
+    name: Yup.string().required(),
+    
+  });
+ 
+  
+
+  try{
+    Schema.validateSync(request.body,{abortEarly:false})
+  }catch(err){
+    return response.status(400).json({ error: err.errors})
+  }
+
+
+  const { name} = request.body
+
+  const category = await Category.create({
+    name,
+  })
+  return response.status(201).json(category)
+ }
+ async index(request,response){
+  const category = await Category.findAll()
+
+  return response.json(category)
+ }
+}
+
+export default new CategoryController()
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import authMiddleware from "./middleware/auth"
 import UserController from "./app/controllers/UserController"
 import SessionController from "./app/controllers/SessionController"
 import ProductController from "./app/controllers/ProductController"
+import CategoryController from "./app/controllers/CategoryController"
 
 const routes = new Router()
 const upload = multer(multerConfig)
@@ -14,8 +15,13 @@ routes.post('/users', UserController.store)
 routes.post('/session', SessionController.store)
 
 routes.use(authMiddleware);
+
 routes.post('/products', upload.single('file') , ProductController.store)
 routes.get('/products', ProductController.index)
+
+routes.post ('/categories', CategoryController.store);
+routes.get ('/categories', CategoryController.index);
+
 
 
 export default routes
